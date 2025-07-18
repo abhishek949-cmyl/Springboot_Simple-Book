84b8d1c0ba645dc1626861903944ece6bf42e493
abhishek949-cmyl <84076911+abhishek949-cmyl@users.noreply.github.com>
2025-07-18 17:53:41 +0530

    Add files via upload

---
+++ Demoapp/src/main/java/com/Abhi/Repositories/BookDtaSrc.java
@@ -0,0 +1,5 @@
+package com.Abhi.Repositories;
+
+public interface BookDtaSrc {
+    String findBookTitleById(Long id);
+}
---
+++ Demoapp/src/test/java/com/Abhi/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.Abhi;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
